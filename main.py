3e1ba5ab24663b3a84531550fb6b458db38b5936
shredder42 <atbinder42@gmail.com>
2025-06-21 10:55:51 -0500

    input

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-)
-print(response.text)
+    args = sys.argv[1:]
 
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=user_prompt,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
