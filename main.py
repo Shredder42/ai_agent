402ca6f768708b38c61d87ea218abb6e8520f4d3
shredder42 <atbinder42@gmail.com>
2025-06-21 11:02:31 -0500

    messages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -14,13 +15,21 @@ def main():
         print('\nUsage: python main.py "prompt here"')
         print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+
     user_prompt = " ".join(args)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt,
+        contents=messages,
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
